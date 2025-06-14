3a8f8ea71e70b46a54e2c8c4d9581c095bfb6a29
Heiner Monge <heinermongelopez@gmail.com>
2025-06-14 15:31:29 -0600

    Quick sort

--- Basic Algorithms/sorting/src/main.zig
+++ Basic Algorithms/sorting/src/main.zig
@@ -2,38 +2,48 @@ const std = @import("std");
 const sorting = @import("sorting");
 
 pub fn main() !void {
-    std.debug.print("Testing sorting algos.\n", .{});
+    std.debug.print("Testing sorting algorithms.\n", .{});
 
     var arenaAllocator = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     const allocator = arenaAllocator.allocator();
 
     defer arenaAllocator.deinit();
 
-    const values = try createRandomArray(allocator, 1000);
+    const values = try allocator.alloc(i32, @intCast(10_000));
+    _ = try populateRandomArray(values);
 
-    _ = bubbleSort(values);
+    //Quick Sort
+    std.debug.print("Quick Sort: \n", .{});
+    quickSort(values, 0, @intCast(values.len - 1));
+    printArray(values);
 
-    for (values) |value| {
-        std.debug.print("{}\n", .{value});
-    }
+    //Bubble Sort
+    _ = try populateRandomArray(values);
+    std.debug.print("Bubble Sort: \n", .{});
+    bubbleSort(values);
+    printArray(values);
 }
 
-fn createRandomArray(allocator: std.mem.Allocator, size: usize) ![]i32 {
-    const values = try allocator.alloc(i32, size);
+fn printArray(array: []i32) void {
+    for (array) |value| {
+        std.debug.print("{} ", .{value});
+    }
+    std.debug.print("\n", .{});
+}
+
+fn populateRandomArray(array: []i32) !void {
     var prng = std.Random.DefaultPrng.init(blk: {
         var seed: u64 = undefined;
         try std.posix.getrandom(std.mem.asBytes(&seed));
         break :blk seed;
     });
 
-    for (values) |*value| {
-        value.* = prng.random().int(i32);
+    for (array) |*i| {
+        i.* = prng.random().int(i32);
     }
-
-    return values;
 }
 
-fn bubbleSort(values: []i32) []i32 {
+fn bubbleSort(values: []i32) void {
     var n: usize = values.len;
     var newn: usize = 0;
     while (n >= 1) {
@@ -48,5 +58,40 @@ fn bubbleSort(values: []i32) []i32 {
             n = newn;
         }
     }
-    return values;
+}
+
+//Quick Sort
+fn quickSort(values: []i32, lo: i64, hi: i64) void {
+    if (lo >= 0 and hi >= 0 and lo < hi) {
+        const pivot = partition(values, lo, hi);
+        quickSort(values, lo, pivot);
+        quickSort(values, pivot + 1, hi);
+    }
+}
+
+fn partition(values: []i32, lo: i64, hi: i64) i64 {
+    const pivot: i32 = values[@intCast(lo)];
+
+    var i: i64 = lo - 1;
+    var j: i64 = hi + 1;
+
+    while (true) {
+        i += 1;
+        while (values[@intCast(i)] < pivot) {
+            i += 1;
+        }
+
+        j -= 1;
+        while (values[@intCast(j)] > pivot) {
+            j -= 1;
+        }
+
+        if (i >= j) {
+            return j;
+        }
+
+        const temp: i32 = values[@intCast(i)];
+        values[@intCast(i)] = values[@intCast(j)];
+        values[@intCast(j)] = temp;
+    }
 }
